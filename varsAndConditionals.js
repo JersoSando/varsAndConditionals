606f4aa3382cf0f9a35eeb2f7a5b632237ea9624
Jerso Sando <jerso.sando@gmail.com>
2021-12-15 12:34:40 -0700

    finished vars & cons demo

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -3,3 +3,49 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+let jonSnowAttack = 25;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+let jamieLannisterAttack = 35;
+
+if (jonSnowAttack > jamieLannisterAttack){
+    console.log("Jon has a better attack than Jamie")
+} else if (jamieLannisterAttack > jonSnowAttack) {
+    console.log("Jamie has a better attack than Jon")
+} else {
+    console.log ('Jon and Jamie have the same attack')
+}
+
+jonSnowDefense += 25
+
+if(jonSnowHealth <= jamieLannisterAttack){
+    console.log("Jon has been slain")
+} else {
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+    console.log(`Jon's health is now ${jonSnowHealth}`)
+}
+
+if ((jonSnowHealth + 50) > 100){
+    jonSnowHealth = 100
+} else {
+    jonSnowHealth += 50
+}
+
+// while(jonSnowHealth > 0){
+//     jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+//     console.log(`Jon's health is ${jonSnowHealth}`)
+//     if(jonSnowHealth <=0){
+//         console.log("Jon has been slain")
+//     }
+// }
+
+for (let i = 0; i < 5; i++){
+    if (jonSnowHealth <= 0){
+        console.log('Jon was slain')
+    } else {
+        jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+        console.log(`Jon Snow's health is ${jonSnowHealth}`)
+    }
+}
